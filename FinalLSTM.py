4e3dc2f31200aea02dc251baa0e13cc7406aa695
Jonathan <jskoning99@gmail.com>
2020-12-04 17:39:04 -0800

    sequence function started

--- FinalLSTM.py
+++ FinalLSTM.py
@@ -15,47 +15,19 @@ import numpy as np
 import spacy
 
 
-class Vocabulary:
-    def __init__(self,freq_threshold):
-        #setting the pre-reserved tokens int to string tokens
-        self.itos = {0:"<PAD>",1:"<SOS>",2:"<EOS>",3:"<UNK>"}
-        
-        #string to int tokens
-        #its reverse dict self.itos
-        self.stoi = {v:k for k,v in self.itos.items()}
-        
-        self.freq_threshold = freq_threshold
-        
-    def __len__(self): return len(self.itos)
-    
-    @staticmethod
-    def tokenize(text):
-        return [token.text.lower() for token in spacy_eng.tokenizer(text)]
-    
-    def build_vocab(self, sentence_list):
-        frequencies = Counter()
-        idx = 4
-        
-        for sentence in sentence_list:
-            for word in self.tokenize(sentence):
-                frequencies[word] += 1
-                
-                #add the word to the vocab if it reaches minum frequecy threshold
-                if frequencies[word] == self.freq_threshold:
-                    self.stoi[word] = idx
-                    self.itos[idx] = word
-                    idx += 1
-    
-    def numericalize(self,text):
-        """ For each word in the text corresponding index token for that word form the vocab built as list """
-        tokenized_text = self.tokenize(text)
-        return [ self.stoi[token] if token in self.stoi else self.stoi["<UNK>"] for token in tokenized_text ]
+def sequence(ratings, reviews, tw):
+    seq = []
+    for i, review in enumerate(reviews):
+        for j in range(len(review)-tw):
+            t_seq = ratings[i]+review[j:j+tw]
+            t_label = review[j+tw]
+            seq.append()
 
 class AmazonDataset(Dataset):
     """
     AmazonDataset
     """
-    def __init__(self,path,freq_threshold=5):
+    def __init__(self,path):
         #read in dataset in json format and convert to csv
         self.df = pd.read_json(path, lines=True)
         self.df = df.to_csv()
@@ -65,8 +37,7 @@ class AmazonDataset(Dataset):
         self.review = self.df["reviewText"]
         
         #Initialize vocabulary and build vocab
-        self.vocab = Vocabulary(freq_threshold)
-        self.vocab.build_vocab(self.review.tolist())
+        
 
         #self.s3 = boto3.resource('s3', region_name='us-west-2')
         
@@ -79,12 +50,9 @@ class AmazonDataset(Dataset):
         review = self.review[idx]       
         
         #numericalize the review text
-        review_vec = []
-        review_vec += [self.vocab.stoi["<SOS>"]]
-        review_vec += self.vocab.numericalize(review)
-        review_vec += [self.vocab.stoi["<EOS>"]]
         
-        return rating, torch.tensor(review_vec)
+        
+        return torch.tensor(review_vec), torch.tensor()
 
 class CapsCollate:
   #Applys padding to the reviews with the dataloader so that the reviews are all the same length
