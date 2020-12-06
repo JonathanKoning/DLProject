f43c8c1b831af22a03cbbcb7bed701f32702206c
Julian Fortune <fortunej@oregonstate.edu>
2020-12-05 20:05:08 -0800

    Embedding update

--- embedding.py
+++ embedding.py
@@ -80,36 +80,28 @@ class Token():
 # model = KeyedVectors.load_word2vec_format("embeddings/crawl-300d-2M.vec")
 
 
-
-def streamReviewsFromCSV(path):
-    with open(path) as csv:
-        for line in csv.readlines():
-            df = pd.read_csv(io.StringIO(line), header=None)
-            review = df[1][0]
-
-            if not pd.notna(review):
-                continue
-
-            if type(review) is not str:
-                review = str(review)
-
-            yield tokenize(review)
-
-
 class AmazonReviewStream(object):
-    def __init__(self, paths):
+    def __init__(self, paths, showEpochs=False):
         self.paths = paths
+        self.showEpochs = showEpochs
         self.epoch = 0
 
     def __iter__(self):
-        self.epoch += 1
-        print(f"  [Epoch {self.epoch}]")
+        if self.showEpochs:
+            self.epoch += 1
+            print(f"  [Epoch {self.epoch}]")
 
         for path in self.paths:
-            print(f"    {path}")
+            print(f"  {path}")
 
             with open(path) as csv:
+                number = 0
                 for line in csv.readlines():
+                    number += 1
+
+                    if number % 1000 == 0:
+                        print(f"    {number} lines...", end="\r")
+
                     df = pd.read_csv(io.StringIO(line), header=None)
                     review = df[1][0]
 
@@ -130,8 +122,8 @@ def learnAmazonEmbedding(architecture="sg", window=5, epochs=3, minFreq=5):
 
     model = 1 if architecture == 'sg' else 0 # 0: Continuous BOW | 1: skip-gram
 
-    trainDirectory = os.path.join(currentDirectory(), "data/amazon/train/")
-    trainPaths = glob.glob(trainDirectory + "*.csv")
+    trainDirectory = os.path.join(currentDirectory(), "data/amazon/csv/")
+    trainPaths = ["data/amazon/csv/1.csv"] # glob.glob(trainDirectory + "*.csv")
 
     assert trainPaths != [], "Unable to find training files!"
 
@@ -155,17 +147,15 @@ def learnAmazonEmbedding(architecture="sg", window=5, epochs=3, minFreq=5):
 
     print(f"\nBuilding vocabulary...")
 
-    word2vec.build_vocab(AmazonReviewStream(trainPaths))
+    word2vec.build_vocab(AmazonReviewStream(trainPaths, showEpochs=False))
 
     print(f"\nTraining...")
 
     word2vec.train(AmazonReviewStream(trainPaths), total_examples=word2vec.corpus_count, epochs=epochs, report_delay=1.0)
 
+    print(f"\Saving...")
 
-def printUsage():
-    print("Error!")
-    print(f"  Usage: python {sys.argv[0]} <amazon/reddit> <sg/cbow>")
-
+    word2vec.save("amazon-300d.model")
 
 def main():
     # corpus = Corpus("corpora/wikitext-2/wiki.train.tokens")
@@ -193,20 +183,4 @@ def main():
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 3:
-        printUsage()
-        exit()
-
-    dataSet = sys.argv[1]
-
-    if dataSet not in ["amazon", "reddit"]:
-        printUsage()
-        exit()
-
-    model = sys.argv[2]
-
-    if dataSet == "amazon":
-        learnAmazonEmbedding(model)
-
-    if dataSet == "reddit":
-        print("Reddit not yet implemented :(")
+    learnAmazonEmbedding("sg")
